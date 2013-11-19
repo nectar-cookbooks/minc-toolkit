7b2855127d7ac426cb280e472bd095e247fab2d6
Stephen Crawley <s.crawley@uq.edu.au>
2013-11-19 16:31:41 +1000

    Fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -36,7 +36,8 @@ if File::exists?("#{bin}/dcm2mnc") then
   return
 end
 
-if platform? == 'centos' then
+case node['platform_family'] 
+when 'rhel'
   deps = [ 'tar', 'gcc-c++', 'make', 'bison', 'flex', 
            'netcdf', 'netcdf-devel', 'hdf5', 'hdf5-devel']
 else 
