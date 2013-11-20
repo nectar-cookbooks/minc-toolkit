d99862ddea4e1639a90d8b2cc509604ebf19ab5f
Stephen Crawley <s.crawley@uq.edu.au>
2013-11-20 10:13:42 +1000

    Fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -27,7 +27,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  
-prefix = node.default['minc-toolkit']['prefix'] | '/usr/local'
+prefix = node.default['minc-toolkit']['prefix'] || '/usr/local'
 bin = "#{prefix}/bin"
 if File::exists?("#{bin}/dcm2mnc") then
   log "Minc tools already installed in #{bin}" do
